e5eb3186886737151bf4edd310ff692315365ded
sarah-am <sarahq.aman@gmail.com>
2019-08-19 17:43:14 +0300

    added pagination

--- main/views.py
+++ main/views.py
@@ -3,19 +3,32 @@ from .models import Article, Contribution, Change
 from .forms import ArticleForm, ContributeArticleForm
 import difflib
 from django.conf import settings
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def articles_list(request):
-    articles = Article.objects.all()
+	articles = Article.objects.all()
 
-    query = request.GET.get("q")
-    if query:
-        articles = articles.filter(title__icontains=query)
+	query = request.GET.get("q")
+	if query:
+		articles = articles.filter(title__icontains=query)
 
-    context = {
-        "articles" : articles,
-    }
-    return render(request, "articles_list.html", context)
+	paginator = Paginator(articles, 10) # Show 10 articles per page
+	page_number = request.GET.get('page')
+
+	try:
+		objects = paginator.page(page_number)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		objects = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		objects = paginator.page(paginator.num_pages)
+
+	context = {
+		"articles" : objects,
+	}
+	return render(request, "articles_list.html", context)
 
 def article_details(request, article_id):
 	context = { "article" : Article.objects.get(id=article_id)}
